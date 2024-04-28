47274f5c433cbba4280a94dd5b4cab5782c62c0f
Virtualtools55 <design.webdev@gmail.com>
2024-04-28 09:36:34 +0530

    75 complet virtual navbar

--- src/app/components/navbar/page.js
+++ src/app/components/navbar/page.js
@@ -27,18 +27,18 @@ export default function Navbar() {
       <Link href="/" className="text-white text-xl font-bold max-md:absolute max-md:left-8 max-md:top-5">logo</Link>
 
 
-      <div className=' max-md:bg-green-500  max-md:w-1/2  max-md:h-screen max-md:absolute max-md:left-0 max-md:top-17'>
-      <ul className={` flex flex-row w-auto text-center rounded-md  justify-evenly bg-red-500 w-96 max-md:flex-col max-md:absolute max-md:left-0 max-md:top-20 max-md:w-72 max-md:h-auto max-md:text-left   ${isOpen ? '' : 'hidden md:block'}`}>
-        <li className='m-3 inline-block text-white max-md:ml-24'>
+      <div className={`max-md:bg-black max-md:w-9/12 max-md:h-screen max-md:absolute max-md:left-0 max-md:top-20  transition-transform duration-5000 ease-in-out transform ${isOpen ? 'translate-x-300' : '-translate-x-full'}  px-4 py-2  ${isOpen ? '' : 'opacity-none md:block'}`}>
+      <ul className={` flex flex-row w-auto text-center rounded-md  justify-evenly  w-96 max-md:flex-col max-md:absolute max-md:left-0 max-md:top-20 max-md:w-64 max-md:h-auto max-md:text-left transition-transform duration-5000 ease-in-out transform ${isOpen ? 'translate-x-300' : '-translate-x-full'}  px-4 py-2 rounded-md ${isOpen ? '' : 'opacity-none md:block'}`}>
+        <li className='m-3 inline-block text-white max-md:ml-20'>
           <Link href="/">Home</Link>
         </li>
-        <li className='m-3 inline-block text-white max-md:ml-24'>
+        <li className='m-3 inline-block text-white max-md:ml-20'>
           <Link href="/">Services</Link>
         </li>
-        <li className='m-3 inline-block text-white max-md:ml-24' >
+        <li className='m-3 inline-block text-white max-md:ml-20' >
           <Link href="/">About</Link>
         </li>
-        <li  className='m-3 inline-block text-white max-md:ml-24'>
+        <li  className='m-3 inline-block text-white max-md:ml-20'>
           <Link href="/">Contact</Link>
         </li>
       </ul>
@@ -47,7 +47,7 @@ export default function Navbar() {
 
       
 
-      <div className={` md:flex w-auto max-md:mt-56 max-md:absolute max-md:top-40 max-md:left-12 ${isOpen ? '' : 'hidden md:block'}`}>
+      <div className={` md:flex w-auto max-md:mt-56 max-md:absolute max-md:top-40 max-md:left-4 transition-transform duration-5000 ease-in-out transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}  px-4 py-2 rounded-md ${isOpen ? '' : 'opacity-none md:block'}`}>
       <button className="text-white bg-red-500 px-4 py-2 rounded-md mr-4">Sign Up</button>
         <button className="text-white bg-red-500 px-4 py-2 rounded-md">Login</button>
 
